0283631a2544d7104c1c80ba1157fb0ed2db0bbb
EstrellitaC <estrellita.caycedo@tecsup.edu.pe>
2023-04-23 19:44:35 -0500

    nuevo

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 	function conexion(){
-	$host = "host=containers-us-west-41.railway.app";
-	$port = "port=6953";
+	$host = "host=containers.railway.app";
+	$port = "port=7710";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
-	$password = "password=gx48a5ALGCFUHSn4bfGb";
+	$password = "password=lOB4ED7jCaKo2om3RToi";
 	$db = pg_connect("$host $port $dbname $user $password");
     return $db;
 }
