ec9774ca2b4a9bdd00ee1052ebbe59eb86d0154b
EstrellitaC <estrellita.caycedo@tecsup.edu.pe>
2023-04-23 21:03:57 -0500

    porfavorlista

--- listar.php
+++ listar.php
@@ -1,12 +1,7 @@
 <?php
-/*
-Este archivo lista todos los datos de la tabla, obteniendo a los mismos como un arreglo
-*/
-?>
-<?php
-include_once "conexion.php";
-$sentencia = $conexion->query("select idpersona, documento, nombre, apellido, direccion, celular from persona");
-$persona = $sentencia->fetchAll(PDO::FETCH_OBJ);
+include_once "conexion.php"; // Incluye el archivo de conexión a la base de datos
+$sentencia = $conexion->query("SELECT idpersona, documento, nombre, apellido, direccion, celular FROM persona"); // Realiza una consulta para obtener todos los registros de la tabla "persona"
+$personas = $sentencia->fetchAll(PDO::FETCH_OBJ); // Obtiene todos los registros de la tabla como un arreglo de objetos
 ?>
 
 <div class="row">
@@ -20,25 +15,29 @@ $persona = $sentencia->fetchAll(PDO::FETCH_OBJ);
 				<thead class="thead-dark">
 					<tr>
 						<th>ID</th>
-                        <th>Persona</th>
+						<th>Documento</th>
 						<th>Nombre</th>
 						<th>Apellido</th>
-                        <th>Direccion</th>
-                        <th>Celular</th>
+						<th>Dirección</th>
+						<th>Celular</th>
+						<th>Editar</th>
+						<th>Eliminar</th>
 					</tr>
 				</thead>
 				<tbody>
 					<!--
 					Modificación
 					-->
-					<?php foreach($persona as $persona){ ?>
+					<?php foreach($personas as $persona){ ?>
 						<tr>
 							<td><?php echo $persona->idpersona ?></td>
 							<td><?php echo $persona->documento ?></td>
 							<td><?php echo $persona->nombre ?></td>
-                            <td><?php echo $persona->apellido ?></td>
-                            <td><?php echo $persona->direccion ?></td>
-                            <td><?php echo $persona->celular ?></td>
+							<td><?php echo $persona->apellido ?></td>
+							<td><?php echo $persona->direccion ?></td>
+							<td><?php echo $persona->celular ?></td>
+							<td><a class="btn btn-warning" href="<?php echo "editar.php?id=" . $persona->idpersona?>">Editar 📝</a></td>
+							<td><a class="btn btn-danger" href="<?php echo "eliminar.php?id=" . $persona->idpersona?>">Eliminar 🗑️</a></td>
 						</tr>
 					<?php } ?>
 				</tbody>
