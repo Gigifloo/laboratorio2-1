ce59b599c460fac97e9074844fa48e80c8e98a7d
EstrellitaC <estrellita.caycedo@tecsup.edu.pe>
2023-04-23 21:15:49 -0500

    lista

--- listar.php
+++ listar.php
@@ -1,47 +1,36 @@
 <?php
-include_once "conexion.php"; // Incluye el archivo de conexión a la base de datos
-$sentencia = $db->query("SELECT idpersona, documento, nombre, apellido, direccion, celular FROM persona"); // Realiza una consulta para obtener todos los registros de la tabla "persona"
-$personas = $sentencia->fetchAll(PDO::FETCH_OBJ); // Obtiene todos los registros de la tabla como un arreglo de objetos
+    //Incluir el archivo de conexión a la base de datos
+    include("conexion.php");
+
+    //Consulta SQL para obtener todos los registros de la tabla "persona"
+    $sql = "SELECT * FROM persona";
+
+    //Ejecutar la consulta y almacenar el resultado en una variable
+    $resultado = pg_query(conexion(), $sql);
+
+    //Si la consulta devuelve algún resultado
+    if(pg_num_rows($resultado) > 0){
+        echo "<table>";
+        echo "<tr><th>ID</th><th>Documento</th><th>Nombre</th><th>Apellido</th><th>Dirección</th><th>Celular</th></tr>";
+
+        //Recorrer los registros y mostrarlos en una tabla HTML
+        while($fila = pg_fetch_assoc($resultado)){
+            echo "<tr>";
+            echo "<td>" . $fila["idpersona"] . "</td>";
+            echo "<td>" . $fila["documento"] . "</td>";
+            echo "<td>" . $fila["nombre"] . "</td>";
+            echo "<td>" . $fila["apellido"] . "</td>";
+            echo "<td>" . $fila["direccion"] . "</td>";
+            echo "<td>" . $fila["celular"] . "</td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "No se encontraron registros";
+    }
+
+    //Cerrar la conexión a la base de datos
+    pg_close(conexion());
 ?>
 
-<div class="row">
-
-	<div class="col-12">
-		<h1>Listar con arreglo</h1>
-		<a href="https://youtu.be/2xI5SyusvBY" target="_blank">Por Estrellita Caycedo</a>
-		<br>
-		<div class="table-responsive">
-			<table class="table table-bordered">
-				<thead class="thead-dark">
-					<tr>
-						<th>ID</th>
-						<th>Documento</th>
-						<th>Nombre</th>
-						<th>Apellido</th>
-						<th>Dirección</th>
-						<th>Celular</th>
-						<th>Editar</th>
-						<th>Eliminar</th>
-					</tr>
-				</thead>
-				<tbody>
-					<!--
-					Modificación
-					-->
-					<?php foreach($personas as $persona){ ?>
-						<tr>
-							<td><?php echo $persona->idpersona ?></td>
-							<td><?php echo $persona->documento ?></td>
-							<td><?php echo $persona->nombre ?></td>
-							<td><?php echo $persona->apellido ?></td>
-							<td><?php echo $persona->direccion ?></td>
-							<td><?php echo $persona->celular ?></td>
-							<td><a class="btn btn-warning" href="<?php echo "editar.php?id=" . $persona->idpersona?>">Editar 📝</a></td>
-							<td><a class="btn btn-danger" href="<?php echo "eliminar.php?id=" . $persona->idpersona?>">Eliminar 🗑️</a></td>
-						</tr>
-					<?php } ?>
-				</tbody>
-			</table>
-		</div>
-	</div>
-</div>
